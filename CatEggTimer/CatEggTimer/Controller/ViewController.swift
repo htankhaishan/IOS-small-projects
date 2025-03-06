d7a09e1f9b99fff34790921236f88cb765f71f24
htankhaishan <htankhaishan@gmail.com>
2025-03-07 00:49:32 +0800

    Section 13

---
+++ CatEggTimer/CatEggTimer/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatEggTimer
+//
+//  Created by khai on 06/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
---
+++ CatEggTimer/CatEggTimerTests/CatEggTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatEggTimerTests.swift
+//  CatEggTimerTests
+//
+//  Created by khai on 06/03/2025.
+//
+
+import Testing
+@testable import CatEggTimer
+
+struct CatEggTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
